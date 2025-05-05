3c9a7d153b2dee673817544783a02e884b5050ea
Hannah <hgsouthall@gmail.com>
2025-05-05 12:47:33 -0400

    began working on connecting to results page - issues with returning the title with spaces - need to change to underscores?

--- movie.js
+++ movie.js
@@ -1,14 +1,20 @@
-console.log("start")
-
-
 async function getMovies() {
     const movies = await fetch ("https://www.omdbapi.com/?apikey=ed5ec5a7&s=jurassic")
     const movieData = await movies.json()
     const movieCardEl = document.querySelector(".movies-short")
-    movieCardEl.innerHTML = movieData
-        .Search.map((movie) =>
-            `
-            <div class="movie__card-short">
+    movieCardEl.innerHTML = movieData.Search.map((movie) => movieHTML(movie)).join("");
+}
+
+getMovies()
+
+
+function showMovieDetails(title) {
+    console.log(title)
+}
+
+
+function movieHTML(movie) {
+    return  `<div class="movie__card-short" onclick= "showMovieDetails(${movie.Title})">
             <div class="movie__title--container-short">
                 <h2 class="movie__title-short">${movie.Title}</h2>
                 <h4 class="release__date">${movie.Year}</h4>
@@ -17,9 +23,4 @@ async function getMovies() {
                 </figure>
             </div>  
         </div>`
-            ).join("")
 }
-getMovies()
-
-
-console.log("end")
