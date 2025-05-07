607c92f20732b6f1a9c1be2428da90a7ef0f92a2
Hannah <hgsouthall@gmail.com>
2025-05-06 22:09:31 -0400

    got background working and connected click on home page to results page. Still need to work out the search function.

--- movie.js
+++ movie.js
@@ -1,20 +1,34 @@
-async function getMovies() {
-    const movies = await fetch ("https://www.omdbapi.com/?apikey=ed5ec5a7&s=jurassic")
+async function onSearchChange (event) {
+    const imdbID = event.target.value;
+    const movies = await fetch ("https://www.omdbapi.com/?apikey=ed5ec5a7&s=${searchWord}")
     const movieData = await movies.json()
-    const movieCardEl = document.querySelector(".movies-short")
-    movieCardEl.innerHTML = movieData.Search.map((movie) => movieHTML(movie)).join("");
 }
 
+
+
+
+
+async function getMovies() {
+    const movies = await fetch ("https://www.omdbapi.com/?apikey=ed5ec5a7&s=jaws")
+    const movieData = await movies.json()
+    const movieCardEl = document.querySelector(".movies-short")
+    console.log(movies)
+    console.log(movieData)
+    movieCardEl.innerHTML = movieData.Search.map((movie) => movieHTML(movie)).join("");
+}
 getMovies()
 
 
-function showMovieDetails(title) {
-    console.log(title)
+
+function showMovieDetails (imdbID) {
+    localStorage.setItem("imdbID", imdbID)
+    window.location.href = `${window.location.origin}/results.html`
 }
 
 
 function movieHTML(movie) {
-    return  `<div class="movie__card-short" onclick= "showMovieDetails(${movie.Title})">
+    return  `<div class="movie__card-short" onclick= "showMovieDetails('${movie.imdbID}')">
+
             <div class="movie__title--container-short">
                 <h2 class="movie__title-short">${movie.Title}</h2>
                 <h4 class="release__date">${movie.Year}</h4>
@@ -24,3 +38,12 @@ function movieHTML(movie) {
             </div>  
         </div>`
 }
+
+async function getDetails() {
+    const results = await fetch("https://www.omdbapi.com/?apikey=ed5ec5a7&t=jaws")
+    const resultsData = await results.json()
+    console.log (resultsData)
+    const backgroundImage = document.querySelector("img.background__image")
+    backgroundImage.src = resultsData.Poster
+}
+getDetails()
--- results.js
+++ results.js
@@ -1,72 +1,70 @@
-console.log("start")
-
-
 async function getMovies() {
-    const movies = await fetch ("https://www.omdbapi.com/?apikey=ed5ec5a7&t=jurassic_park")
-    const movieData = await movies.json()
+    const imdbID = localStorage.getItem("imdbID")
+    const result = await fetch (`https://www.omdbapi.com/?apikey=ed5ec5a7&i=${imdbID}`)
+    const resultData = await result.json()
+    console.log(resultData)
     const movieCardEl = document.querySelector(".movies")
-    console.log(movies)
-    console.log(movieData)
-    movieCardEl.innerHTML = movieData
-        .Search.map((movie) =>
-            `
-            <div class= "movie__card">
-                <div class="movie__title--container">
-                    <h2 class="movie__title">${movie.Title}</h2>
-                    <h4 class="release__date">${movie.Year}</h4>
-                </div>
-                <div class="movie__subtitle--container">
-                    <div class="movie___keywords">keyword | keyword | keyword</div>
-                    <div class="rating__container">
-                        <h4>rating</h4>
-                        <div class="stars-or-percent">****</div>
-                    </div>
-                </div>
-                <hr class="solid">
-                <div class="movie__details--container">
-                    <figure class="poster__container">
-                        <img class="poster" src="${movie.Poster}" alt="">
-                    </figure>
-                    <div class="movie__people--container">
-                        <div class="movie__people--wrapper">
-
-                                <h4>director</h4>
-                                <p class="director__name">${movie.Director}</p>
-                        </div>
-                        <div class="movie__people--wrapper">
-                                <h4>writers</h4>
-                                <p class="writers__name">writers names</p>
-                        </div>
-                        <div class="movie__people--wrapper">
-                                <h4>actors</h4>
-                                <p class="actors__name">actor names</p>
-                        </div>
-                    </div>
-                    <div class="movie__left--container">
-                        <div class="summary__container">
-                            <h4 class="summary">Summary</h4>
-                            <p class="summary">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo molestiae dolorum provident laborum ea quisquam itaque. Aperiam iusto facere ipsa?</p>
-                        </div>
-                        <div class="movie__left--subcontainer">
-                            <div class="movie__details--small">
-                                <h4 class="smaller__subtitles">Awards</h4>
-                                <p class="awards">text text text</p>
-                            </div>
-                            <div class="movie__details--small">
-                                <h4 class="smaller__subtitles">Awards</h4>
-                                <p class="awards">text text text</p>
-                            </div>
-                            <div class="movie__details--small">
-                                <h4 class="smaller__subtitles">Awards</h4>
-                                <p class="awards">text text text</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>`
-            ).join("")
+    movieCardEl.innerHTML = resultDetailsHTML(resultData) 
 }
 getMovies()
 
 
-console.log("end")
+
+
+function resultDetailsHTML (result) {
+    return  `
+    <div class= "movie__card">
+        <div class="movie__title--container">
+            <h2 class="movie__title">${result.Title}</h2>
+            </div>
+            <div class="movie__subtitle--container">
+                <h4 class="release__date">${result.Year}</h4>
+                <h4 class="movie___keywords">${result.Genre}</h4>
+                <div class="rating__container">
+                    <h4>Rated: ${result.Rated}</h4>
+                </div>
+            </div>
+        <hr class="solid">
+        <div class="movie__details--container">
+            <figure class="poster__container">
+                <img class="poster" src="${result.Poster}" alt="">
+            </figure>
+            <div class="movie__people--container">
+                <div class="movie__people--wrapper">
+                        <h4>Director</h4>
+                        <p class="director__name">${result.Director}</p>
+                </div>
+                <div class="movie__people--wrapper">
+                        <h4>Writers</h4>
+                        <p class="writers__name">${result.Writer}</p>
+                </div>
+                <div class="movie__people--wrapper">
+                        <h4>Actors</h4>
+                        <p class="actors__name">${result.Actors}</p>
+                </div>
+            </div>
+            <div class="movie__left--container">
+                <div class="summary__container">
+                    <h4 class="summary">Summary</h4>
+                    <p class="summary">${result.Plot}</p>
+                </div>
+                <div class="movie__left--subcontainer">
+                    <div class="movie__details--small">
+                        <h4 class="smaller__subtitles">Awards</h4>
+                        <p class="awards">${result.Awards}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`
+}
+
+async function getDetails() {
+    const imdbID = localStorage.getItem("imdbID")
+    const results = await fetch(`https://www.omdbapi.com/?apikey=ed5ec5a7&i=${imdbID}`)
+    const resultsData = await results.json()
+    console.log (resultsData)
+    const backgroundImage = document.querySelector("img.background__image")
+    backgroundImage.src = resultsData.Poster
+}
+getDetails()
